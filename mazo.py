6056733ba31fac5fcd91bc31becdedadaf1f34ea
ontoligent <ontoligent@gmail.com>
2020-03-26 08:29:54 -0400

    Various mods.

--- mazo.py
+++ mazo.py
@@ -22,8 +22,8 @@ wher <k> stands for the number of topics in the model.
 import sys, os, re
 from polite.polite import Polite
 
-
 mallet_bin = 'mallet' # NEEDS TO BE CHANGED WITH ENV
+mallet_output_dir = './output'
 
 # Get keyword
 try:
@@ -55,21 +55,23 @@ if not os.path.isfile(mallet_file):
     print("Done creating MALLET file.")
 
 # Make sure output directory exists
-if not os.path.isfile('./output'):
+if not os.path.isfile(mallet_output_dir):
     try:
-        os.mkdir('./output')
+        os.mkdir(mallet_output_dir)
     except FileExistsError:
         pass
 
-if not os.path.isfile('./output/{}'.format(keyword)):
+if not os.path.isfile('{}/{}'.format(mallet_output_dir, keyword)):
     try:
-        os.mkdir('./output/{}'.format(keyword))
+        os.mkdir('{}/{}'.format(keyword))
     except FileExistsError:
         pass
 
 # Run the topic model
 print("Running topic model.")
 # Eventually provide ways to override these defaults
+trial_key = os.utime()
+mallet_trial_dir = "{}/{}-{}".format(mallet_output_dir, keyword, trial_key)
 params = {
     'num-topics': n_topics,
     'num-top-words': 10,
@@ -80,14 +82,14 @@ params = {
     'doc-topics-max': 10,
     'show-topics-interval': 100,
     'input': mallet_file,
-    'output-topic-keys': 'output/{}/topic-keys.txt'.format(keyword),
-    'output-doc-topics': 'output/{}/doc-topics.txt'.format(keyword),
-    'word-topic-counts-file': 'output/{}/word-topic-counts.txt'.format(keyword),
-    'topic-word-weights-file': 'output/{}/topic-word-weights.txt'.format(keyword),
-    'xml-topic-report': 'output/{}/topic-report.xml'.format(keyword),
-    'xml-topic-phrase-report': 'output/{}/topic-phrase-report.xml'.format(keyword),
-    'diagnostics-file': 'output/{}/diagnostics.xml'.format(keyword),
-    'output-state': 'output/{}/output-state.gz'.format(keyword)
+    'output-topic-keys': '{}/topic-keys.txt'.format(mallet_trial_dir),
+    'output-doc-topics': '{}/doc-topics.txt'.format(mallet_trial_dir),
+    'word-topic-counts-file': '{}/word-topic-counts.txt'.format(mallet_trial_dir),
+    'topic-word-weights-file': '{}/topic-word-weights.txt'.format(mallet_trial_dir),
+    'xml-topic-report': '{}/topic-report.xml'.format(mallet_trial_dir),
+    'xml-topic-phrase-report': '{}/topic-phrase-report.xml'.format(mallet_trial_dir),
+    'diagnostics-file': '{}/diagnostics.xml'.format(mallet_trial_dir),
+    'output-state': '{}/output-state.gz'.format(mallet_trial_dir)
 }
 cmds = []
 for k, v in params.items():
